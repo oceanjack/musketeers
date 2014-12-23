9d4597102d7602256946f509d84e2894d12b11fc
oceanjack <oceanjack.app@gmail.com>
2014-12-24 03:10:59 +0800

    update

--- scripts/settings.js
+++ scripts/settings.js
@@ -38,6 +38,11 @@ var addContentToDataBase = function() {
     url: 'more.html'
   }
   ];
+  var mockc = {
+    content: "<h3>要任性</h3>\r\n<span class='time'>2014.12.23</span>\r\n<p>今天是个好天气，那么我该做些什么呢？</p>\r\n<img src='imgs/miao.jpg'/>\r\n<p>没错！就是睡觉，运筹帷幄之中！总有一天走上人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！</p>\r\n<p>然后羽化而登仙了!</p>"
+  };
+
+
   for(var i = 0, l = mock.length; i < l; ++i) {
     var ArticleList = AV.Object.extend("ArticleList");
     var articleList = new ArticleList();
@@ -45,6 +50,7 @@ var addContentToDataBase = function() {
     articleList.set("title", mock[i].title);
     articleList.set("like", mock[i].like);
     articleList.set("url", mock[i].url);
+    articleList.set("detail", mockc.content);
 
     articleList.save(null, {
       success: function(articleList) {
--- scripts/showMore.js
+++ scripts/showMore.js
@@ -1,21 +1,32 @@
 //显示文章更多内容
 var showMore = function() {
-  var mock = {
-    content: "<h3>要任性</h3>\r\r<span class='time'>2014.12.23</span>\r\n<p>今天是个好天气，那么我该做些什么呢？</p>\r\n<img src='imgs/miao.jpg'/>\r\n<p>没错！就是睡觉，运筹帷幄之中！总有一天走上人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！</p>\r\n<p>然后羽化而登仙了!</p>"
-  };
-  var content = [];
-  var index = mock.content.indexOf('\n');
-  while(index != -1) {
-    content.push(mock.content.substr(0, index));
-    mock.content = mock.content.substr(index + 1, mock.content.length);
-    index = mock.content.indexOf('\n');
+  var id = window.location.href.split('?')[1].substr(3);
+  var ArticleList = ArticleListInit();
+  var query = new AV.Query(ArticleList);
+  query.get(id, {
+    success: function(mock) {
+      show(mock.attributes.detail);
+    },
+    error: function(res) {
+      window.location.href = "index.html";
+    }
+  });
+
+  var show = function(mock) {
+    var content = [];
+    var index = mock.indexOf('\n');
+    while(index != -1) {
+      content.push(mock.substr(0, index));
+      mock = mock.substr(index + 1, mock.length);
+      index = mock.indexOf('\n');
+    }
+    if(mock.trim()) {
+      content.push(mock);
+    }
+    var article = $('<div class="article"></div>');
+    for(var i = 0, l = content.length; i < l; ++i) {
+      article.append($(content[i]));
+    }
+    $('.container').append(article);
   }
-  if(mock.content.trim()) {
-    content.push(mock.content);
-  }
-  var article = $('<div class="article"></div>');
-  for(var i = 0, l = content.length; i < l; ++i) {
-    article.append($(content[i]));
-  }
-  $('.container').append(article);
 }();
