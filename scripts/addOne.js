cfa718cec22cf7ba04fcbe5aa71bfc4e0e98617b
oceanjack <oceanjack.app@gmail.com>
2014-12-24 16:02:41 +0800

    update

--- scripts/addOne.js
+++ scripts/addOne.js
@@ -1,34 +1,35 @@
 //添加首页文章
 var addContentToDataBase = function() {
-  var mock = [{
-      img: 'imgs/1.png',
-      title: '测试下',
-      like: 0,
-      url: 'more.html'
+  var img, title, content;
+  $('.container .addOne button').on('click', function() {
+    img = $('.container .addOne input').eq(0).val();
+    title = $('.container .addOne input').eq(1).val();
+    content = $('.container .addOne textarea').val();
+    if(img.trim() == '') {
+      img = 'imgs/1.png';
     }
-  ];
-  var mockc = {
-    content: (
-      "<h3>要任性哈</h3>\r\n" + 
-      "<span class='time'>2014.12.23</span>\r\n" + 
-      "<p>今天是个好天气，那么我该做些什么呢？</p>\r\n" + 
-      "<img src='imgs/miao.jpg'/>\r\n" + 
-      "<p>没错！就是睡觉，运筹帷幄之中！总有一天走上人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！人生巅峰！</p>\r\n" + 
-      "<p>然后羽化而登仙了!</p>"
-    )
-  };
-
-  for(var i = 0, l = mock.length; i < l; ++i) {
+    if(title.trim() == '') {
+      title = '无题';
+    }
+    if(content.trim() == '') {
+      alert('请输入文章HTML');
+      return;
+    }
+    add();
+  });
+  
+  var add = function() {
     var ArticleList = AV.Object.extend("ArticleList");
     var articleList = new ArticleList();
-    articleList.set("img", mock[i].img);
-    articleList.set("title", mock[i].title);
-    articleList.set("like", mock[i].like);
-    articleList.set("url", mock[i].url);
-    articleList.set("detail", mockc.content);
+    articleList.set("img", img);
+    articleList.set("title", title);
+    articleList.set("like", 0);
+    articleList.set("url", 'more.html');
+    articleList.set("detail", content);
 
     articleList.save(null, {
       success: function(articleList) {
+        window.location.href = 'more.html?id=' + articleList.id;
       },
       error: function(articleList, error) {
         alert('' + error.description);
